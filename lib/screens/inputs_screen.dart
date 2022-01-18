4afc98e334f5819dd254a93db51b1db3c15ed456
AbnerJosuEstrada <josueestrada@twowheelstogo.com>
2022-01-17 20:58:51 -0600

    firt commit

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
